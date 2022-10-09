407535db9313a842f3a84e82bc11002127f8104c
Slavnikov Bogdan <baizy228@mail.ru>
2022-10-09 11:44:07 +1000

    new hooks

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -5,17 +5,29 @@ import Users from "./layouts/users";
 import Login from "./layouts/login";
 import Main from "./layouts/main";
 import NavBar from "./components/ui/navBar";
+import { ToastContainer } from "react-toastify";
+import { ProfessionProvider } from "./hooks/useProfession";
+import { QualitiesProvider } from "./hooks/useQualities";
 
 function App() {
     return (
         <div>
             <NavBar />
-            <Switch>
-                <Route path="/users/:userId?/:edit?" component={Users} />
-                <Route path="/login/:type?" component={Login} />
-                <Route path="/" exact component={Main} />
-                <Redirect to="/" />
-            </Switch>
+            <QualitiesProvider>
+                <ProfessionProvider>
+                    <Switch>
+                        <Route
+                            path="/users/:userId?/:edit?"
+                            component={Users}
+                        />
+                        <Route path="/login/:type?" component={Login} />
+
+                        <Route path="/" exact component={Main} />
+                        <Redirect to="/" />
+                    </Switch>
+                </ProfessionProvider>
+            </QualitiesProvider>
+            <ToastContainer />
         </div>
     );
 }
